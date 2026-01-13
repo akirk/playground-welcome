9d94e6f3bbbe1d7154c0e4d84783208b09c35b20
Alex Kirk <akirk@users.noreply.github.com>
2026-01-13 23:20:20 +0100

    Add a vision page

--- playground-welcome.php
+++ playground-welcome.php
@@ -49,20 +49,49 @@ class Playground_Welcome {
         return '';
     }
 
-    public static function activate() {
-        $post = get_post(1);
-        if (!$post) {
-            return;
+    public static function get_vision_page_path() {
+        $locale = determine_locale();
+        $lang = substr($locale, 0, 2);
+        $plugin_dir = plugin_dir_path(__FILE__);
+
+        $localized_file = $plugin_dir . $lang . '/vision-page.html';
+        if (file_exists($localized_file)) {
+            return $localized_file;
         }
 
+        return $plugin_dir . 'vision-page.html';
+    }
+
+    public static function get_vision_page_content() {
+        $path = self::get_vision_page_path();
+        if (file_exists($path)) {
+            return file_get_contents($path);
+        }
+        return '';
+    }
+
+    public static function activate() {
         add_filter('wp_kses_allowed_html', [__CLASS__, 'allow_svg_tags']);
 
-        wp_update_post([
-            'ID' => $post->ID,
-            'post_title' => __('Welcome to Your WordPress', 'playground-welcome'),
-            'post_content' => self::get_welcome_post_content(),
-            'post_name' => 'welcome-to-your-wordpress',
-        ]);
+        $post = get_post(1);
+        if ($post) {
+            wp_update_post([
+                'ID' => $post->ID,
+                'post_title' => __('Welcome to Your WordPress', 'playground-welcome'),
+                'post_content' => self::get_welcome_post_content(),
+                'post_name' => 'welcome-to-your-wordpress',
+            ]);
+        }
+
+        $sample_page = get_post(2);
+        if ($sample_page) {
+            wp_update_post([
+                'ID' => $sample_page->ID,
+                'post_title' => __('The Vision', 'playground-welcome'),
+                'post_content' => self::get_vision_page_content(),
+                'post_name' => 'the-vision',
+            ]);
+        }
     }
 
     public static function allow_svg_tags($tags) {
